16be72515e95e136dbcf5a5fb48afa88ee8d43ec
Shawn Sutherland <shawn@graphiti.com>
2021-03-05 13:35:56 -0800

    change paths

--- wp-config.php
+++ wp-config.php
@@ -16,8 +16,8 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 // ========================
 // Custom Content Directory
 // ========================
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'https://' . $_SERVER['HTTP_HOST'] . '/content' );
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+define( 'WP_CONTENT_URL', 'https://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
 
 // ================================================
 // You almost certainly do not want to change these
@@ -29,14 +29,14 @@ define( 'DB_COLLATE', '' );
 // Salts, for security
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
 // ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '||je0A|#Np}xUMoxpFBCFAVTE8SDj_nfaj[&kVf~6Ndjo044[!QA|dU,+Is&56 F');
+define('SECURE_AUTH_KEY',  'jjKl_UV!#_Uw2 tdK.NZw<DXQ:iW<RK0pMKjN#;RR4 _oLOYd#uW)`TdPYs.y I$');
+define('LOGGED_IN_KEY',    'tWjC+txO j_.aRQ YP*E-Nh-#xbEi}rCLhI.&54fXX<*=F?_3(Dr_F|)!u_-K3nQ');
+define('NONCE_KEY',        'E%4K@K|;p%4U$<|fh1AY_h4SW+-c{Le6|x|(&~?p;@$ LaZJr2Khkh91st-<jny>');
+define('AUTH_SALT',        'i:(`1yb5a.xf-s|A.JpKP1$<z1<v+d;Ye*;]0EMEy+9<2+)t|i~0}<Ti9nf+C]eV');
+define('SECURE_AUTH_SALT', '/-TFt?IfH*g{MsJuJ[=mzG</uu7IYTFN3bf$:X|?tE*6zL{T[uzc}W8C#(sOTuS(');
+define('LOGGED_IN_SALT',   'QR}OA|>k}Rvvg@43O+]{dqYxi4l)T&4+#1]^OR&E+fLf0J.,,Si/NK#hFQ9&8ZCa');
+define('NONCE_SALT',       '<bjch+]uY#;VEA}~=v-s4@yMvDs~QYyil-MjTEo7fCQ>Q%8s-|hYt_(,$)VG|VA+');
 
 // ==============================================================
 // Table prefix
@@ -79,5 +79,5 @@ define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack t
 // Bootstrap WordPress
 // ===================
 if ( !defined( 'ABSPATH' ) )
-	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wordpress/' );
 require_once( ABSPATH . 'wp-settings.php' );
